10ad3cd7336c677720d5e422aadd27b2bda0239a
ARU0007 <aruneshagrahari95@gmail.com>
2024-02-16 20:06:25 +0530

    added comment for implimentation of wrapper class

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         //creating object and declaring generic type.
+        //must use wrapper class not the primitv inside the <>.
         //Second declaration is optional.
         Printer <Integer> Printer = new Printer<>(95);
 
